07f17cbfb54efea3375c838f69cde5ccfd410a6b
atsu-i <euglena115@gmail.com>
2022-06-28 19:59:00 +0900

    日本語入力でエンターキーで変換を確定できるように変更

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -52,7 +52,8 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState("");
-  console.log({ disabled, string });
+  const [isComposed, setisComposed] = useState(false);
+  // console.log({ disabled, string, isComposed });
   useEffect(() => {
     const disabled = string === "";
     setDisabled(disabled);
@@ -78,6 +79,20 @@ export default function SignIn({ setName }) {
             name="name"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              if (isComposed) return;
+
+              if (e.key === "Enter") {
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
+            onCompositionStart={() => {
+              setisComposed(true);
+            }}
+            onCompositionEnd={() => {
+              setisComposed(false);
+            }}
           />
           <Button
             type="button"
