ae28cfc32aa64e81ff21952c3f5312b98b7efbc3
atsu-i <euglena115@gmail.com>
2022-06-28 20:08:57 +0900

    config.jsonを作成

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Main from "./Main";
 import SignIn from "./SignIn";
+import config from "../config";
+// console.log({ config });
 
 export default () => {
   const [name, setName] = useState("");
-  console.log({ name });
-  if (name === "") {
+  // console.log({ name });
+  if (config.signInEnabled && name === "") {
     return <SignIn setName={setName} />;
   } else {
     return <Main name={name} />;
