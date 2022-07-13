803794cbf9543cacf70e1db9861071efa2043933
atsu-i <euglena115@gmail.com>
2022-07-13 20:11:09 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,22 @@
 import firebase from "firebase";
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
 const firebaseConfig = {
-  apiKey: "AIzaSyB7Sj0-XOv1v3joOiJxNq78fTkXxYv83Nk",
-  authDomain: "chat-app-c6ad5.firebaseapp.com",
-  projectId: "chat-app-c6ad5",
-  storageBucket: "chat-app-c6ad5.appspot.com",
-  messagingSenderId: "821851199839",
-  appId: "1:821851199839:web:a4aedf69edda703158feea",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
