86df626fd8a79d25e2b4d937033e8b5cbec30f9e
atsu-i <euglena115@gmail.com>
2022-07-27 18:15:26 +0900

    adjust-focusing

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 import { pushMessage } from "../firebase";
 
-export const MessageField = ({ name, setText, text }) => {
+export const MessageField = ({ inputEl, name, setText, text }) => {
   const [isComposed, setisComposed] = useState(false);
   return (
     <TextField
+      autoFocus
       fullWidth={true}
+      inputRef={inputEl}
       onChange={(e) => {
         setText(e.target.value);
       }}
--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -3,13 +3,14 @@ import { IconButton } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 import { pushMessage } from "../firebase";
 
-const MessageSubmitButton = ({ name, setText, text }) => {
+const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
   return (
     <IconButton
       disabled={text === ""}
       onClick={() => {
         pushMessage({ name, text });
         setText("");
+        inputEl.current.focus();
       }}
     >
       <SendIcon />
